23dac375b72f218a59f8bbe5d3c8e59440c0f627
Heldt Daniel <Daniel.Heldt@lm.se>
2017-01-26 08:57:44 +0100

    börjat fulkod

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # legacy
+
+## Krav
+Programet ska
+  - Validera fälten
+  - Filtrera ut minderåriga
+
+## CSV Filen
+Filen har fyra kolumner med data. Förnamn, efternamn, personnummer och epost
+
+    firstname;lastname;personal_number;email
+    Vaughan;Stafford;16290828-0361;Nunc@ipsum.net
+    
+    
+## Programet
+
+    java -jar legacy.jar <fil-sökväg> <århundrade>
+    
+    java -jar legacy.jar /tmp/fil 1600
+    
---
+++ src/main/java/se/knowit/legay/Legacy.java
@@ -0,0 +1,40 @@
+package se.knowit.legay;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Legacy {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("start");
+
+        File file = new File(args[0]);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String str = null;
+        int first = 0;
+        while ((str = br.readLine()) != null) {
+            if(first == 0) {
+                first++;
+                continue;
+            }
+            System.out.println(str);
+            String[] cls = str.split(";");
+            //valid email
+            Pattern pattern = Pattern.compile("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
+
+            Matcher match = pattern.matcher(cls[3]);
+
+            if (!match.matches()) {
+                System.out.println(cls[3]); // bort
+                throw new Exception("FEL MAIL");
+            }
+
+            //valid
+        }
+
+    }
+}
